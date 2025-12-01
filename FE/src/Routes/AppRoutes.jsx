eb245a644062e99159f178d4b459e0e83329a75d
aradhyacp <cparadhya@proton.me>
2025-12-01 18:21:04 +0530

    feat: add Success and Cancel pages for stripe gateway with routing

--- FE/src/Routes/AppRoutes.jsx
+++ FE/src/Routes/AppRoutes.jsx
@@ -9,6 +9,8 @@ import Root from "../Pages/Root";
 import Signup from "../Pages/Signup";
 import Login from "../Pages/Login";
 import Dashboard from "../Pages/Dashboard";
+import Success from "../Pages/Success";
+import Cancel from "../Pages/Cancel";
 
 const AppRoutes = () => {
     const router = createBrowserRouter(createRoutesFromElements(
@@ -17,6 +19,8 @@ const AppRoutes = () => {
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/stripe/success" element={<Success/>}/>
+        <Route path="/stripe/cancel" element={<Cancel/>}/>
         </>
     ));
     return <RouterProvider router={router}/>
